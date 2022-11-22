4a1fecb5262b89b20adfc7b4dc99987476c7a9c0
Robin Persson <robinpn98@hotmail.se>
2022-11-22 11:24:22 +0100

    added form input & buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,16 @@
-const form = document.querySelector("form")
-const 
+// variables
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const Ulist = document.querySelector('ul');
+const clearBtn = document.querySelector('.clear');
+const luckyBtn = document.querySelector('.lucky');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let userInput = input.value;
+  let li = document.createElement('li');
+  li.textContent = userInput;
+  Ulist.append(li);
+
+  input.value = '';
+});
