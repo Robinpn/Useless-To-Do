56795b729b8215d0178b62728f2c674bca5e7e44
Robin Persson <robinpn98@hotmail.se>
2022-12-12 13:28:54 +0100

    everything

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,92 @@
 // variables
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const Ulist = document.querySelector('ul');
+const uList = document.querySelector('ul');
 const clearBtn = document.querySelector('.clear');
 const luckyBtn = document.querySelector('.lucky');
+const duplaicateBtn = document.querySelector('.clear');
+
+//Duplicate all li elements and print them out
+duplaicateBtn.addEventListener('click', () => {
+  const items = uList.querySelectorAll('li');
+  items.forEach((item) => {
+    addItem(item.textContent);
+  });
+});
+
+const addItem = (text) => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = text;
+  uList.append(li);
+};
+
+const consonant = [
+  'b',
+  'c',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'v',
+  'z',
+  'x',
+];
+
+//get a random consonant
+const getConsonant = (arr) => {
+  const randomConsonant = Math.floor(Math.random() * arr.length);
+  const cons = arr[randomConsonant];
+
+  return cons;
+};
+
+const luckyArr = [
+  'ignorera räkningar',
+  'mata plantan',
+  'vattna Fisken',
+  'stryka Sängen',
+  'tvätta Hunden',
+  'torka Disken',
+  'diska Maten',
+  'skura Fönster',
+];
+
+const luckyPrint = () => {
+  luckyBtn.addEventListener('click', () => {
+    luckyArr.forEach((random) => {
+      addItem(random);
+    });
+  });
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let userInput = input.value;
-  let li = document.createElement('li');
-  li.textContent = userInput;
-  Ulist.append(li);
+  const userInput = input.value;
 
   input.value = '';
+
+  //replace all vowels with random consonant
+  const changeVowel = (string) => {
+    const newString = string.replaceAll(
+      /["a""e""i""o""u""y""å""ä""ö""A""E""I""O""U""Y""Å""Ä""Ö"]/g,
+      getConsonant(consonant)
+    );
+    console.log(newString);
+    return newString;
+  };
+
+  addItem(changeVowel(userInput));
 });
+
+luckyPrint();
